8da0fc34ebbb850f480c2e8975968e77bc922e15
Dyllan Santos <dyllanlima17@gmail.com>
2023-07-02 14:03:58 -0300

    Atualização

--- atendimento_email.php
+++ atendimento_email.php
@@ -11,49 +11,53 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+    if(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['assunto'])){
+        echo 'Preencha todos os campos!!';
+        header('refresh:2;url=atendimento.html');
+    }else{
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $assunto = $_POST['assunto'];
 
-    // Verifica se o botão foi pressionado
-    if(!empty($_POST['submit'])){
-        echo 'Por favor, preencha os campos!!';
-    }
+//Instanciando a váriavel do email  
+$mail = new PHPMailer(true);     //Instancia do PHPmailer
 
-    $mail = new PHPMailer(true); // Instancia do PHPMailer
-
-    try{
-        $mail = isSMTP();
-        $mail->SMTPSecure = 'tls';                                  //Enviar usando TLS
-        $mail->Host       = 'smtp.gmail.com';                     //Servidor usado
-        $mail->SMTPAuth   = true;                                   //Ativando autenticacao SMTP
-        $mail->Username   = 'testeprojeto40@gmail.com';                     //Usuario SMTP
-        $mail->Password   = 'mzacqpqnulwnuyrr';                           //Senha SMTP     
-        $mail->Port       = 587;        //Porta usada para TLS
+//Fazendo a ligação do email
+try {
+    //Configurações do servidor (gmail)
     
-        //Aqui ele tira o erro do SSL e da conexão com o Host
-        $mail->SMTPOptions = array(
-        'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-        )
-        );
-        
-        //quem envia e recebe
-        $mail->setFrom($email);  //Usuario SMTP e Nome aleatório
-        $mail->addAddress('testeprojeto40@gmail.com');     //Email do Destinatario
-        $mail->isHTML(true);  //Habilitando o uso do HTML
-        $mail->charset = 'UTF-8';
-        $mail->Subject = $nome;    //Titulo
-        $mail->Body    = $assunto;   //Corpo
-        $mail->AltBody = $assunto;
-        $mail->send();
-    
-    } catch (Exception $e) {
-        echo 'Erro: '.$e->getMessage();
-        header('Location: index.html');
-    }
+    $mail->isSMTP();      
+    $mail->SMTPSecure = 'tls';                                  //Enviar usando TLS
+    $mail->Host       = 'smtp.gmail.com';                     //Servidor usado
+    $mail->SMTPAuth   = true;                                   //Ativando autenticacao SMTP
+    $mail->Username   = 'testeprojeto40@gmail.com';                     //Usuario SMTP
+    $mail->Password   = 'mzacqpqnulwnuyrr';                               //Senha SMTP     
+    $mail->Port       = 587;        //Porta usada para TLS
 
+    //Aqui ele tira o erro do SSL e da conexão com o Host
+    $mail->SMTPOptions = array(
+    'ssl' => array(
+    'verify_peer' => false,
+    'verify_peer_name' => false,
+    'allow_self_signed' => true
+    )
+    );
+    
+    //quem envia e recebe
+    $mail->setFrom($email,$nome);  //Usuario SMTP e Nome aleatório
+    $mail->addAddress('testeprojeto40@gmail.com');     //Email do Destinatario
+    $mail->isHTML(true);                                  //Habilitando o uso do HTML
+    $mail->charset = 'UTF-8';
+    $mail->Subject = "SUPORTE $email";    //Titulo
+    $mail->Body    = "Nome: $nome<br>Email: $email<br>Mensagem: $assunto";
+    $mail->AltBody =  "Nome: $nome<br>Email: $email<br>Mensagem: $assunto";
+    $mail->send();
+    header('refresh:2;url=atendimento.html');
+    
+} catch (Exception $e) {
+    echo "Mensagem não foi enviada. ERRO: {$mail->ErrorInfo}";   //Mensagem de erro, depois envia para o
+    header('refresh:2;url=atendimento.html');
+}
+}
 ?>
---
+++ recuperar.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+
+require_once __DIR__. '/lib/vendor/autoload.php';
+require_once __DIR__. '/lib/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require_once __DIR__. '/lib/vendor/phpmailer/phpmailer/src/SMTP.php';
+require_once __DIR__. '/lib/vendor/phpmailer/phpmailer/src/Exception.php';
+require 'conexao.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+/*Verifica se o campo está preenchido corretamente */
+
+if(empty($_POST['email'])){
+    echo 'Preencha o campo!';
+    header('refresh:2;url=recuperar.html');
+}
+
+/* Aqui recebe o email digitado pelo usuário */
+$email = $_POST['email'];
+
+/* Selecionando o usuario com o email */
+$sql = "SELECT * FROM usuario WHERE Email='$email'";
+
+/* Executa o SQL */
+$result = $conn->query($sql);
+
+/*Criando matriz para a consulta */
+$row = $result->fetch_assoc();
+
+/*Se ele achar o email no BD, ele executa o procedimento */
+if($result->num_rows > 0){
+    
+    $mail = new PHPMailer(true);
+
+    try{
+        $mail->isSMTP();      
+    $mail->SMTPSecure = 'tls';                                  //Enviar usando TLS
+    $mail->Host       = 'smtp.gmail.com';                     //Servidor usado
+    $mail->SMTPAuth   = true;                                   //Ativando autenticacao SMTP
+    $mail->Username   = 'testeprojeto40@gmail.com';                     //Usuario SMTP
+    $mail->Password   = 'mzacqpqnulwnuyrr';                               //Senha SMTP     
+    $mail->Port       = 587;        //Porta usada para TLS
+
+    //Aqui ele tira o erro do SSL e da conexão com o Host
+    $mail->SMTPOptions = array(
+    'ssl' => array(
+    'verify_peer' => false,
+    'verify_peer_name' => false,
+    'allow_self_signed' => true
+    )
+    );
+    
+    //quem envia e recebe
+    $mail->setFrom('testeprojeto40@gmail.com');  //Usuario SMTP e Nome aleatório
+    $mail->addAddress($email);     //Email do Destinatario
+    $mail->isHTML(true);                                  //Habilitando o uso do HTML
+    $mail->charset = 'UTF-8';
+    $mail->Subject = "Redefinir Senha";    //Titulo
+    $mail->Body    = "Olá!<br><br>Caso queira redefinir sua senha, <a href='http://localhost/recuperando.html?email=$email' target='_blank'>Clique aqui!</a><br><br>Caso não queira, desconsidere este email.";
+    $mail->AltBody =  "Olá!<br><br>Caso queira redefinir sua senha, <a href='http://localhost/recuperando.html?email=$email' target='_blank'>Clique aqui!</a><br><br>Caso não queira, desconsidere este email.";
+    $mail->send();
+    header('refresh:2;url=recuperar.html');
+
+    }catch (Exception $e){
+        echo "Mensagem não foi enviada. ERRO: {$mail->ErrorInfo}";
+        mysqli_close($sql);
+    }
+
+}else{
+    echo 'Email não existe!';
+    mysqli_close($sql);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
